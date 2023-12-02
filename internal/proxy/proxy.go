0cb1a75fd67784db42129e3474986a5473f02bdf
Jahor Laryn <mrojahor@gmail.com>
2023-12-02 23:13:16 +0300

    Improve svg

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -49,49 +49,106 @@ func (p *proxy) Serve(addr string) error {
 	return http.ListenAndServe(addr, nil)
 }
 
+func calculateLevels(nodes []models.Node, edges []models.Edge) map[string]int {
+	levels := make(map[string]int)
+	visited := make(map[string]bool)
+	var dfs func(string, int)
+	dfs = func(nodeName string, level int) {
+		if visited[nodeName] {
+			return
+		}
+		visited[nodeName] = true
+		if level > levels[nodeName] {
+			levels[nodeName] = level
+		}
+		for _, edge := range edges {
+			if edge.Source == nodeName {
+				dfs(edge.Destination, level+1)
+			}
+		}
+	}
+	for _, node := range nodes {
+		if _, exists := visited[node.Name]; !exists {
+			dfs(node.Name, 0)
+		}
+	}
+	return levels
+}
+
 func (p *proxy) HandleGraph(w http.ResponseWriter, r *http.Request) {
 	graph := p.graph
 
-	w.Header().Set("Content-Type", "image/svg+xml")
+	levels := calculateLevels(graph.Nodes, graph.Edges)
+	maxLevel := 0
+	for _, l := range levels {
+		if l > maxLevel {
+			maxLevel = l
+		}
+	}
 
+	w.Header().Set("Content-Type", "image/svg+xml")
 	width := 800
 	height := 600
 
 	canvas := svg.New(w)
 	canvas.Start(width, height)
-	canvas.Grid(0, 0, width, height, 10, "stroke:gray;opacity:0.8;stroke-width:0.5")
 	canvas.Title(graph.Name)
 
-	// Draw nodes
-	nodeRadius := 20
+	canvas.Def()
+	canvas.Marker("arrow", 10, 10, 10, 10, "orient:auto")
+	canvas.Path("M0,0 L10,5 L0,10 z", "fill:black")
+	canvas.MarkerEnd()
+	canvas.DefEnd()
+
+	xSpacing := width / (maxLevel + 1)
 	nodeCoords := make(map[string][2]int)
 
-	for i, node := range graph.Nodes {
-		x := (i + 1) * 100
-
-		sign := []int{-1, 1}
-		rand.Shuffle(2, func(i, j int) {
-			sign[i], sign[j] = sign[j], sign[i]
-		})
-
-		y := height/2 + (rand.Int()%(height/4))*sign[0]
-
-		nodeCoords[node.Name] = [2]int{x, y}
-
-		canvas.Circle(x, y, nodeRadius, "fill:#ebcb8b")
-		canvas.Text(x, y, node.Name, "text-anchor:middle;font-size:11px;fill:black")
-	}
-
-	// Draw edges
-	for _, edge := range graph.Edges {
-		if srcCoords, ok := nodeCoords[edge.Source]; ok {
-			if dstCoords, ok := nodeCoords[edge.Destination]; ok {
-				canvas.Line(srcCoords[0], srcCoords[1], dstCoords[0], dstCoords[1], "stroke:black")
-				midX := (srcCoords[0] + dstCoords[0]) / 2
-				midY := (srcCoords[1] + dstCoords[1]) / 2
-				canvas.Text(midX, midY, "+", "text-anchor:middle;font-size:11px;fill:black")
+	// Сначала проинициализируем координаты узлов
+	for level := 0; level <= maxLevel; level++ {
+		nodesInLevel := make([]models.Node, 0)
+		for _, node := range graph.Nodes {
+			if levels[node.Name] == level {
+				nodesInLevel = append(nodesInLevel, node)
 			}
 		}
+		ySpacing := height / (len(nodesInLevel) + 1)
+		for i, node := range nodesInLevel {
+			x := (level + 1) * xSpacing
+			y := (i + 1) * ySpacing
+			nodeCoords[node.Name] = [2]int{x, y}
+		}
+	}
+
+	// Рисуем линии
+	nodeRadius := 20
+
+	for _, edge := range graph.Edges {
+		srcCoords := nodeCoords[edge.Source]
+		dstCoords := nodeCoords[edge.Destination]
+
+		if slices.Equal(srcCoords[:], []int{0, 0}) || slices.Equal(dstCoords[:], []int{0, 0}) {
+			continue
+		}
+
+		canvas.Line(srcCoords[0], srcCoords[1], dstCoords[0], dstCoords[1], "stroke:black;marker-end:url(#arrow)")
+		midX := (srcCoords[0] + dstCoords[0]) / 2
+		midY := (srcCoords[1] + dstCoords[1]) / 2
+
+		text := getNodeByName(edge.Source, p.graph.Nodes).Output
+		textWidth := len(text) * 4 // Подбор числа для оценки ширины текста
+		textHeight := 10           // Высота текста, может потребовать подгонки
+
+		canvas.Rect(midX-textWidth/2, midY-textHeight/2-2, textWidth, textHeight, fmt.Sprintf("fill:%s;stroke:black;stroke-width:1", randomSecondaryColour()))
+		canvas.Text(midX, midY, text, fmt.Sprintf("text-anchor:middle;font-size:%dpx;fill:black", textHeight-4))
+	}
+
+	// Рисуем узлы поверх линий и метки рёбер
+	for _, node := range graph.Nodes {
+		x := nodeCoords[node.Name][0]
+		y := nodeCoords[node.Name][1]
+
+		canvas.Circle(x, y, nodeRadius, fmt.Sprintf("fill:%s;stroke:black;stroke-width:1", randomColour()))
+		canvas.Text(x, y+5, node.Name, "text-anchor:middle;font-size:11px;fill:black")
 	}
 
 	canvas.End()
@@ -224,4 +281,22 @@ func ConvertToAnyMap(inputMap map[string]proto.Message) (map[string]*anypb.Any, 
 	}
 
 	return outputMap, nil
+}
+
+func randomColour() string {
+	colors := []string{
+		"#bf616a",
+		"#ebcb8b",
+		"#ebcb8b",
+		"#a3be8c",
+		"#81a1c1",
+	}
+	return colors[rand.Intn(len(colors))]
+}
+
+func randomSecondaryColour() string {
+	colors := []string{
+		"#88c0d0",
+	}
+	return colors[rand.Intn(len(colors))]
 }
